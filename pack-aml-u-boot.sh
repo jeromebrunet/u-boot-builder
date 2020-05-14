313150c71320794f21ac4f8b2cffc4b2a7ec041b
Jerome Brunet <jbrunet@baylibre.com>
2020-05-15 01:36:49 +0200

    script: add packing helper
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

---
+++ pack-aml-u-boot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+# Quick and dirty packing script for concourse CI
+
+#1 is the u-boot directory
+#2 is the fip directory
+#3 is the out directory
+#4 is the platform name
+
+# Get the git version
+pushd $1
+GITVERS=`git describe`
+popd
+
+# Generate the atf
+$2/generate-bins.sh $2 $1/u-boot.bin $3 atf.bin
+
+# make the release tarball
+pushd $3
+OUTNAME="atf-$4-$GITVERS"
+mkdir $OUTNAME
+cp atf.bin* $OUTNAME
+tar czf $OUTNAME.tar.gz $OUTNAME
+popd
